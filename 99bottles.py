8e2559f0ee5507a9e85296525695476777e95212
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2017-11-08 23:12:49 +0100

    First working version!

--- 99bottles.py
+++ 99bottles.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
 
 for i in range(99, 0, -1):  # range from 99 to 1
-    print(i)
+    print(i, "bottles of beer on the wall,", i, "bottles of beer,",
+          "take one down, pass it around,", i-1, "bottles of beer on the wall")
