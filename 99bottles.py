bb53912e1b50e710c60424041b89ca64eda27aac
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2017-11-08 23:34:38 +0100

    Add a comma

--- 99bottles.py
+++ 99bottles.py
@@ -5,5 +5,5 @@ otw = "on the wall"
 todpia = "take one down, pass it around,"
 
 for i in range(99, 0, -1):  # range from 99 to 1
-    print(i, bob, otw, i, bob + ",",
+    print(i, bob, otw + ",", i, bob + ",",
           todpia, i-1, bob, otw)
