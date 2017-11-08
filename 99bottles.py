1d59e290a28f07c13092b0cb67c0319b760a0897
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2017-11-08 23:44:25 +0100

    add a sad ASCII monkey at the end

--- 99bottles.py
+++ 99bottles.py
@@ -2,3 +2,13 @@
 
 for i in range(99, 0, -1):  # range from 99 to 1
     print("%s bottles of beer on the wall, %s bottles of beer, take one down, pass it around, %s bottles of beer on the wall" % (i, i, i-1))
+
+print("""
+      .-"-.
+    _/.-.-.\\_
+   ( ( o o ) )
+    |/  "  \\|
+     \\ .-. /
+     /`\"\"\"`\\
+    /       \\
+      """)
