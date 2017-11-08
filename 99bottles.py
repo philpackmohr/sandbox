afbe194039f44c7620c8c6ec00b56b2e43d64026
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2017-11-08 23:53:42 +0100

    Add exclamation marks on master branch

--- 99bottles.py
+++ 99bottles.py
@@ -7,7 +7,7 @@ todpia = "take one down, pass it around,"
 for i in range(99, 0, -1):  # range from 99 to 1
 
     print(i, bob, otw + ",", i, bob + ",",
-          todpia, i-1, bob, otw)
+          todpia, i-1, bob, otw + "!!!")
 
 print("""
       .-"-.
