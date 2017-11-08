aedbb5da77f094424fa061ae78bbbc39a954421f
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2017-11-08 23:31:14 +0100

    Switch to %s string notation

--- 99bottles.py
+++ 99bottles.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
 
 for i in range(99, 0, -1):  # range from 99 to 1
-    print(i, "bottles of beer on the wall,", i, "bottles of beer,",
-          "take one down, pass it around,", i-1, "bottles of beer on the wall")
+    print("%s bottles of beer on the wall, %s bottles of beer, take one down, pass it around, %s bottles of beer on the wall" % (i, i, i-1))
